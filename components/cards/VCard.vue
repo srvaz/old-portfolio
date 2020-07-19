33102692a3fb2f118fb63231b23199d925dfc7d3
srvaz <jonwify@gmail.com>
2020-07-19 15:24:29 -0300

    :lipstick: Add VCard component
    This is a base card component.

---
+++ components/cards/VCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <component
+    :is="tag"
+    :class="{
+      'v-card--decorated': decorated,
+    }"
+    class="v-card"
+  >
+    <slot />
+  </component>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component({
+  name: 'VCard',
+})
+export default class VCard extends Vue {
+  @Prop({
+    type: Boolean,
+    default: false,
+  })
+  decorated!: boolean
+
+  @Prop({
+    type: String,
+    default: 'div',
+  })
+  tag!: string
+}
+</script>
+
+<style lang="scss" scoped>
+.v-card {
+  background-color: #fff;
+  border: 1px solid unquote('rgb(var(--vs-gray-4))');
+  border-radius: var(--vs-radius);
+  padding: $space-st;
+  position: relative;
+
+  &.v-card--decorated {
+    border: none;
+    box-shadow: 0 0 $space-st $color-shadow;
+
+    &::before {
+      content: '';
+      border-radius: var(--vs-radius);
+      position: absolute;
+      top: -$space-st;
+      left: -$space-st;
+      width: 100%;
+      height: 100%;
+      background-image: url('~assets/img/dot.svg');
+      z-index: -1;
+    }
+  }
+}
+</style>
