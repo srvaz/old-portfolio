12f52c39e478c2ebd678f47a9156f9df2dda772a
srvaz <jonwify@gmail.com>
2020-07-19 15:23:03 -0300

    :art: Standardize style lang on project
    Add standard lang (SCSS) in lang attribute of style tags.

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,8 +4,9 @@
   </div>
 </template>
 
-<style>
+<style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Montserrat:100,100italic,200,200italic,300,300italic,regular,italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic');
+@import url('boxicons/css/boxicons.min.css');
 
 html {
   font-family: 'Montserrat', 'Source Sans Pro', -apple-system,
@@ -17,6 +18,7 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+  scroll-behavior: smooth;
 }
 
 *,
@@ -24,5 +26,10 @@ html {
 *::after {
   box-sizing: border-box;
   margin: 0;
+}
+
+body {
+  scroll-behavior: smooth;
+  @include color-theme('background-color', 'light');
 }
 </style>
